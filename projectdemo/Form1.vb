a1c0fa856a872443093a524c7ae55687126d5c7b
joyeta <joyeta.sardar2015@gmail.com>
2017-04-12 11:16:38 +0530

    ruru

--- projectdemo/Form1.vb
+++ projectdemo/Form1.vb
@@ -39,4 +39,8 @@
         Label1.Visible = False
         ComboBox1.Visible = False
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
--- projectdemo/Form3.vb
+++ projectdemo/Form3.vb
@@ -69,4 +69,8 @@ Public Class Form3
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
 
     End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
 End Class
