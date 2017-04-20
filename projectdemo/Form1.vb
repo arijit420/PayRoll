f1d4ecd1c0e0dbfcd6c399558cdf7471452745f7
joyeta99 <joyeta.sardar2015@gmail.com>
2017-04-20 20:35:45 +0530

    added the form.

--- projectdemo/Form1.vb
+++ projectdemo/Form1.vb
@@ -1,3 +1,13 @@
 ﻿Public Class Form1
+    Private Sub EXITToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles EXITToolStripMenuItem.Click
+        End
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GroupBox1.Visible = False
+    End Sub
+
+    Private Sub ADDEMPToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ADDEMPToolStripMenuItem.Click
+        GroupBox1.Visible = True
+    End Sub
 End Class
