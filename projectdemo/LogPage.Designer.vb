0230b5604e4a8d4e184414dc540903010cbc6360
Arijit <arijitpanigrahi10@gmail.com>
2017-05-05 23:56:00 +0530

    Login from database added

--- projectdemo/LogPage.Designer.vb
+++ projectdemo/LogPage.Designer.vb
@@ -29,11 +29,12 @@ Partial Class LogPage
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.RadioButton2 = New System.Windows.Forms.RadioButton()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.login = New System.Windows.Forms.Button()
         Me.reset = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.ComboBox1 = New System.Windows.Forms.ComboBox()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.login = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label2
@@ -72,7 +73,7 @@ Partial Class LogPage
         Me.RadioButton1.AutoSize = True
         Me.RadioButton1.BackColor = System.Drawing.Color.Transparent
         Me.RadioButton1.Font = New System.Drawing.Font("Modern No. 20", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RadioButton1.Location = New System.Drawing.Point(177, 465)
+        Me.RadioButton1.Location = New System.Drawing.Point(177, 454)
         Me.RadioButton1.Name = "RadioButton1"
         Me.RadioButton1.Size = New System.Drawing.Size(49, 20)
         Me.RadioButton1.TabIndex = 5
@@ -85,7 +86,7 @@ Partial Class LogPage
         Me.RadioButton2.AutoSize = True
         Me.RadioButton2.BackColor = System.Drawing.Color.Transparent
         Me.RadioButton2.Font = New System.Drawing.Font("Modern No. 20", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RadioButton2.Location = New System.Drawing.Point(227, 465)
+        Me.RadioButton2.Location = New System.Drawing.Point(227, 454)
         Me.RadioButton2.Name = "RadioButton2"
         Me.RadioButton2.Size = New System.Drawing.Size(82, 20)
         Me.RadioButton2.TabIndex = 6
@@ -98,21 +99,11 @@ Partial Class LogPage
         Me.Label4.AutoSize = True
         Me.Label4.BackColor = System.Drawing.Color.Transparent
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(53, 465)
+        Me.Label4.Location = New System.Drawing.Point(53, 454)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(83, 17)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "User Type"
-        '
-        'login
-        '
-        Me.login.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.login.Location = New System.Drawing.Point(99, 640)
-        Me.login.Name = "login"
-        Me.login.Size = New System.Drawing.Size(75, 26)
-        Me.login.TabIndex = 8
-        Me.login.Text = "Login"
-        Me.login.UseVisualStyleBackColor = True
         '
         'reset
         '
@@ -154,6 +145,28 @@ Partial Class LogPage
         Me.TextBox2.Size = New System.Drawing.Size(153, 33)
         Me.TextBox2.TabIndex = 12
         '
+        'login
+        '
+        Me.login.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.login.Location = New System.Drawing.Point(99, 640)
+        Me.login.Name = "login"
+        Me.login.Size = New System.Drawing.Size(75, 26)
+        Me.login.TabIndex = 8
+        Me.login.Text = "Login"
+        Me.login.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.BackColor = System.Drawing.Color.Transparent
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label5.Location = New System.Drawing.Point(376, 645)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(300, 16)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Operator  Forgot Password ?? (Click Here)"
+        '
         'LogPage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -161,7 +174,8 @@ Partial Class LogPage
         Me.BackColor = System.Drawing.Color.SteelBlue
         Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1246, 737)
+        Me.ClientSize = New System.Drawing.Size(1246, 676)
+        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.ComboBox1)
         Me.Controls.Add(Me.Label1)
@@ -187,9 +201,10 @@ Partial Class LogPage
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents RadioButton2 As RadioButton
     Friend WithEvents Label4 As Label
-    Friend WithEvents login As Button
     Friend WithEvents reset As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents ComboBox1 As ComboBox
     Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents login As Button
+    Friend WithEvents Label5 As Label
 End Class
--- projectdemo/LogPage.vb
+++ projectdemo/LogPage.vb
@@ -10,8 +10,6 @@
         Label1.Visible = False
         ComboBox1.Visible = False
         Me.WindowState = FormWindowState.Maximized
-
-
     End Sub
 
     Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
@@ -28,26 +26,44 @@
                 MsgBox("Please check your Userid & Password", vbCritical, "Login Error!")
             End If
         ElseIf (RadioButton2.Checked = True) Then
-            If (ComboBox1.SelectedIndex = 0 AndAlso TextBox1.Text = "k_operator" AndAlso TextBox2.Text = "1234") Then
-                Me.Hide()
-                wlcmopearor.Show()
-            ElseIf (ComboBox1.SelectedIndex = 1 AndAlso TextBox1.Text = "m_operator" AndAlso TextBox2.Text = "1234") Then
-                Me.Hide()
-                wlcmopearor.Show()
-            ElseIf (ComboBox1.SelectedIndex = 2 AndAlso TextBox1.Text = "d_operator" AndAlso TextBox2.Text = "1234") Then
-                Me.Hide()
-                wlcmopearor.Show()
-            ElseIf (ComboBox1.SelectedIndex = 3 AndAlso TextBox1.Text = "d_operator" AndAlso TextBox2.Text = "1234") Then
-                Me.Hide()
-                wlcmopearor.Show()
+            If TextBox1.Text = "" Then
+                MessageBox.Show("Please enter your Student User Name to login", "Payroll Login", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox1.Focus()
+            ElseIf TextBox2.Text = "" Then
+                MessageBox.Show("Please enter your Password to login", "Payroll Login", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                TextBox2.Focus()
+            ElseIf ComboBox1.Text = "" Then
+                MessageBox.Show("Please select your Branch to login", "Payroll Login", MessageBoxButtons.OK, MessageBoxIcon.Error)
             Else
-                MsgBox("Please check your Userid & Password", vbCritical, "Login Error!")
+                Dim conLogin As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\DELL\Documents\vb_net.accdb")
+                Dim commLogin As New OleDb.OleDbCommand("SELECT * FROM login_operator WHERE ID = @ID AND Password = @Password And Branch = @Branch", conLogin)
+                Dim usernameParam As New OleDb.OleDbParameter("@ID", Me.TextBox1.Text)
+                Dim passwordParam As New OleDb.OleDbParameter("@Password", Me.TextBox2.Text)
+                Dim statusParam As New OleDb.OleDbParameter("@Branch", Me.ComboBox1.SelectedItem)
+
+                commLogin.Parameters.Add(usernameParam)
+                commLogin.Parameters.Add(passwordParam)
+                commLogin.Parameters.Add(statusParam)
+                commLogin.Connection.Open()
+                Dim reader As OleDb.OleDbDataReader = commLogin.ExecuteReader()
+                If reader.HasRows Then
+                    wlcmopearor.Show()
+                    Me.Hide()
+                Else
+                    MessageBox.Show("Invalid  Data! Please enter correct  Data.", "Payroll Login", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    TextBox1.Clear()
+                    TextBox2.Clear()
+                    ComboBox1.Text = ""
+                    TextBox1.Focus()
+                End If
+                commLogin.Connection.Close()
             End If
         Else
             MsgBox("Please choose the user type!", vbInformation, "Login error")
         End If
         TextBox1.Text = ""
         TextBox2.Text = ""
+        ComboBox1.Text = ""
         RadioButton1.Checked = False
         RadioButton2.Checked = False
     End Sub
